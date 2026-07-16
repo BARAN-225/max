b6994eefe59c7baa76797bda66cd6ee9f00b1d27
BARAN-225 <BK1428615@GMAIL.COM>
2026-07-16 16:36:51 +0530

    nn

--- main.py
+++ main.py
@@ -148,4 +148,4 @@ def register_user(payload: RegisterRequest):
 if __name__ == "__main__":
     import uvicorn
     # Points to this file name. Assuming you name this file `main.py`
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
